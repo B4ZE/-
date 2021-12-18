c6fa872ba227e55289c3e799f8164a0527302eaa
B4ZE <95387149+B4ZE@users.noreply.github.com>
2021-12-18 21:35:24 +0100

    Update z1.c

--- z1.c
+++ z1.c
@@ -6,8 +6,6 @@ int main()
     scanf("%d %d",&M,&N);
     int arr[N+2][M+2];
     int arr2[N+2][M+2];
-    //arr2[5][1]=216;
-    //printf("%d",arr[0][0]);//ako se vnese za M:3, N:5 togash se dobiva 216 koja e isto se arr2[6][1]
     for(int y=0; y<M+2; y++)
     {
         for(int x=0; x<N+2; x++)
@@ -27,23 +25,8 @@ int main()
     {
         for(int x=0; x<N+2; x++)
         {
-            //printf(" .%d. ",arr[1][1]);
             arr2[x][y]=arr[x][y];//koga x=6 y=2 arr[1][1] se promenuva
-            /*if(arr[1][1]!=1)
-            {
-                printf("\neve go x:%d,y:%d,arr[1][1]:%d",x,y,arr[1][1]);
-                printf("\nnizata:\n");
-                for(int j=0;j<M+2;j++)
-                {
-                    for(int i=0;i<N+2;i++)
-                    {
-                        printf("%d ",arr[i][j]);
-                    }
-                    printf("\n");
-                }
-                system("pause");
-                return 0;
-            }*/
+            
         }
     }
     for(int y=1; y<M+1; y++)
